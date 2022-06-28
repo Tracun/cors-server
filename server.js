bf78dae60d6240369f0075b1c1dc8092e90a8604
Lucas Waiteman Bastos <lucas060994@gmail.com>
2022-06-28 14:22:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:51571', 'https://gianini-manutencao.web.app'],
+    originWhitelist: ['http://localhost:51571', 'https://gianini-manutencao.web.app', 'https://gianini-manutencao-dev.web.app'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
